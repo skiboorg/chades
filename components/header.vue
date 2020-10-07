3995238f75b6914b03455fac362c82f8b1178f62
xxx <11@11.11>
2020-10-07 21:35:38 +0300

    fix tests

--- components/header.vue
+++ components/header.vue
@@ -13,7 +13,7 @@
                     <a class="header-nav__item" href="#">联络</a>
                 </div>
                 <div v-if="!this.$auth.loggedIn" class="header-btn">
-                   <nuxt-link  to="/auth" class="btn">登录或注册</nuxt-link>
+                   <nuxt-link  to="/entrance" class="btn">登录或注册</nuxt-link>
                  </div>
               <div v-else class="header-btn">
                  <span  @click="$auth.logout()" class="btn">从您的个人资料中删除</span>
@@ -23,7 +23,7 @@
               </div>
             </div>
           <div class="mobile-menu" :class="{'menuActive':menuOpen}">
-            <nuxt-link  to="/auth" class="btn mb-25">登录或注册</nuxt-link>
+            <nuxt-link  to="/entrance" class="btn mb-25">登录或注册</nuxt-link>
             <nuxt-link to="/courses" class="mobile-menu__item">我们的课程</nuxt-link>
             <nuxt-link to="\" class="mobile-menu__item">关于</nuxt-link>
             <a class="header-nav__item" href="#">费率</a>
@@ -77,7 +77,7 @@
           this.bg = '/test.png'
         }
 
-        if(url === '/auth'){
+        if(url === '/entrance'){
           this.indexPage = false
           this.noHeader = true
           this.bg = ''
